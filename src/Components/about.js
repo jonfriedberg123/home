7d7b5a6180710abfb8fad4fa8e24e0f833d42df2
friedbergjon <friedberg_jon@hotmail.com>
2019-08-28 11:41:21 -0400

    typo

--- src/Components/about.js
+++ src/Components/about.js
@@ -11,11 +11,11 @@ function about() {
      </div>
      <div className="SkillsAndSoftware">
        <h2 className= "SkillsTitle">Skills</h2>
-     <p className="SkillsParagraphText">Front-End/Back-End/Full Stack Development, HTML, CSS & CSS libraries, PostgreSQL, MongoDB Javascript, R, Python, React, Node.js, Express, Ruby, Drawing, Painting, 3-D Modeling, UV Unwrapping, Storyboarding, Photography, Stereo & 5.1 Recording, Mixing & Editing; Automated Dialogue Replacement; SFX (Foley, Soft FX, Hard FX); Onset Sound; Appropriate dB Ranges for Broadcast Standards; Studio Recording, Mixing, & Editing; Microphone Placement; Signal Flow; Live Sound Reinforcement; Soldering
+     <p className="SkillsParagraphText">Front-End/Back-End/Full Stack Development, HTML, CSS & CSS libraries, PostgreSQL, MongoDB, Javascript, R, Python, React, Node.js, Express, Ruby, Drawing, Painting, 3-D Modeling, UV Unwrapping, Storyboarding, Photography, Stereo & 5.1 Recording, Mixing & Editing; Automated Dialogue Replacement; SFX (Foley, Soft FX, Hard FX); Onset Sound; Appropriate dB Ranges for Broadcast Standards; Studio Recording, Mixing, & Editing; Microphone Placement; Signal Flow; Live Sound Reinforcement; Soldering
     </p>
     
       <h2 className= "SoftwareTitle">Software</h2>
-    <p className="SoftwareParagraphText">Visual Studio, Maya, Z-Brush, 3D Studio Max, Marvellous Designer, Substance Painter, Final Cut Pro, Adobe Suite: Premiere; Audition; Illustrator; Photoshop; After Effects; In Design, TextMate, ProTools, Logic Pro, Reason, Symphonic Orchestra, Symphonic Choirs, Word Builder, Garage Band, DVD Compressor
+    <p className="SoftwareParagraphText">Text Editors, Maya, Z-Brush, 3D Studio Max, Marvellous Designer, Substance Painter, Final Cut Pro, Adobe Suite: Premiere; Audition; Illustrator; Photoshop; After Effects; In Design, TextMate, ProTools, Logic Pro, Reason, Symphonic Orchestra, Symphonic Choirs, Word Builder, Garage Band, DVD Compressor
     </p>  
     </div>
     </div>
