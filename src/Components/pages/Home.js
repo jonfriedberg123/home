62deeadb516991f6a6d5ebd9c3fdbf4f36ed7e82
friedbergjon <friedberg_jon@hotmail.com>
2019-10-10 15:21:23 -0400

    update

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Apple from "../../Images/threeDObjects/Apple.gif";
-
-
 function Home() {
     return (
      
-     <div className="homePagePicture">
-      
-       <img className="fixed_img_home" src={Apple}/> 
+     <div className="home_page_div">
+       <img className="home_page_picture" src={Apple}/> 
  
-    </div>
+        </div>
     )
 }
 
