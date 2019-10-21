c9582e860e83d86e661e4ba68ed2bb83b65da3df
friedbergjon <friedberg_jon@hotmail.com>
2019-10-21 19:19:41 -0400

    update

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Apple from "../../Images/threeDObjects/Apple.gif";
+import dreamScene from "../../Images/illustrations/dreamScene.jpg";
 function Home() {
     return (
      
      <div className="home_page_div">
-       <img className="home_page_picture" src={Apple}/> 
+       <img className="home_page_picture" src={dreamScene}/> 
  
         </div>
     )
