c1a92efb628fb8435fcb43770b934c67d2c0c905
friedbergjon <friedberg_jon@hotmail.com>
2020-03-13 14:45:02 -0700

    image centered, adding border

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import '../../Style.css';
 import {ResponsiveEmbed,Image } from 'react-bootstrap';
-import Apple from '../../Images/threeDObjects/Apple.gif'
+import Apple from '../../Images/threeDObjects/Apple.gif';
+import AutoFitImage from 'react-image-autofit-frame';
 
 class Home extends Component {
 
     
   render() {
     return (
-      <div 
-      style={{
-      alignItems: 'center', height: '30%', margin: 0, margin:'auto', paddingTop: '5%', paddingBottom: '5%'}}>
-       <img src = {Apple}></img>
-      </div>)
+      
+      <div className="homePic"
+      style={{width: 330, height: 'auto',  display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center', margin: 'auto', paddingTop: '5%', paddingBottom: '5%'}}>
+     <AutoFitImage frameWidth="500px" frameHeight="400px" imgSrc = {Apple}/>
+      </div>
+   )
   }
 };
 
