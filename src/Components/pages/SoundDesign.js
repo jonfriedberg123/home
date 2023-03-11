c8fedeaa1954aa23ea4fc40f4c5b7063aac7e8d1
friedbergjon <37493374+friedbergjon@users.noreply.github.com>
2023-03-10 19:08:11 -0700

    Add files via upload

---
+++ src/Components/pages/SoundDesign.js
@@ -0,0 +1,23 @@
+import React, { Component }from 'react';
+import '../../Style.css';
+import ReactPlayer from 'react-player';
+import audioReel from '../../Videos/audioreel.mp4'
+
+
+
+export const soundDesign = () => (
+ 
+       
+  <div className ='audio-reel'>
+     
+                <ReactPlayer
+                url= {audioReel}
+                controls = {true}
+                width='100%'
+            height='100%'
+                 />
+                
+  </div>
+
+    )
+export default soundDesign;
