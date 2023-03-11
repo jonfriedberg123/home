1f912ffddea52dfd892eb8e2f02fe93d840a9599
Jonathan Friedberg <jonathan.friedberg@Jonathans-MacBook-Air.local>
2023-03-10 19:47:09 -0700

    css and webdev edits added

--- src/Components/pages/WebDevelopment.js
+++ src/Components/pages/WebDevelopment.js
@@ -7,21 +7,20 @@ export const webDevelopment = () => (
   
 
 <div className="webDevLinks">
-  <br></br>
-      <ul>
+<br></br>
+    <ul>
      <div className="LinkOne">
-     <li><a target="_blank" href="https://giftme-hjm.herokuapp.com/">"Wish List App" using React and Express - mobile responsive</a>
+     <li><a target="_blank" href="https://malorbooks.com/"> Malor Books Website - malorbooks.com </a>
+     </li>
+     </div>
+     <br></br>
+     <div className="LinkTwo">
+     <li><a target="_blank" href="https://ishk.net/"> ISHK Website - ishk.net </a>
      </li> 
      </div>
   <br></br>
-     <div className="LinkTwo">
-     {/* https://stackoverflow.com/questions/42914666/react-router-external-link */}
-     <li><a target="_blank" href="https://friedbergjon.github.io/api-currency-converter/">Currency Converter using React</a>
-     </li>
-     </div>
-  <br></br>
-     <div className="LinkThree">
-     <li><a target="_blank" href="https://friedbergjon.github.io/frogger/">"Frogger- The Game" made with Vanila Javascript</a>
+     <div className="LinkFive">
+     <li><a target="_blank" href="https://friedbergjon.github.io/frogger/"> "Frogger" - The Game, made with Vanila Javascript</a>
      </li>
      </div>
      </ul>
